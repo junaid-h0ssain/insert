4a10bc83e5d77d8f330b7ce3e85ebae171d3aef4
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-25 11:59:40 +0600

    sreamlined auth

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,15 +5,29 @@ import { v } from "convex/values";
 export default defineSchema({
     projects: defineTable({
         name: v.string(),
-        // description: v.string().isOptional(),
-        // createdAt: v.date(),
         ownerID: v.string(),
+        updatedAt: v.number(),
         importStatus: v.optional(
             v.union(
                 v.literal("in_progress"),
                 v.literal("completed"),
                 v.literal("failed"),
-            )
+            ),
+        ),
+        exportStatus: v.optional(
+            v.union(
+                v.literal("exporting"),
+                v.literal("completed"),
+                v.literal("failed"),
+                v.literal("cancelled"),
+            ),
+        ),
+        exportRepoUrl: v.optional(v.string()),
+        settings: v.optional(
+            v.object({
+                installCommand: v.optional(v.string()),
+                devCommand: v.optional(v.string()),
+            })
         ),
     }).index("byOwner", ["ownerID"]),
 });
