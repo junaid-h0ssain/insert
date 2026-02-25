8a08293567c11ba1cb18af40b023a476376d476a
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-25 12:05:37 +0600

    project component implementaions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-'use client';
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { use } from "react";
+const Home = () => {
+    return <ProjectsView />
+};
 
-export default function Home() {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-  return (
-    <div>
-      <Button onClick={() => createProject({ name: "New Project" })}>Click me</Button>
-      <div>
-        {projects?.map((project) => (
-          <div key={project._id} className="p-4 border-b">
-            <h2 className="text-lg font-semibold">{project.name}</h2>
-            <p>{project.ownerID}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+export default Home;
